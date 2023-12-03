97e70e66d28b6c32ba61ec37e04d651e1dca86fb
kiko-g <kikojpgoncalves@gmail.com>
2023-12-03 12:31:52 +0000

    fix: website template images

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,8 +1,8 @@
 export function formatDate(dateString: string) {
-  return new Date(`${dateString}T00:00:00Z`).toLocaleDateString('en-US', {
+  return new Date(`${dateString}T00:00:00Z`).toLocaleDateString('pt-PT', {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
-    timeZone: 'UTC',
+    timeZone: 'GMT',
   })
 }
