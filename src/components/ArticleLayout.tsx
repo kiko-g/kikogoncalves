14e5d9571ad60e1bcb55c3bcd595b4db7d4dca83
kiko-g <kikojpgoncalves@gmail.com>
2023-12-07 03:07:05 +0000

    add: articles from gatsby website

--- src/components/ArticleLayout.tsx
+++ src/components/ArticleLayout.tsx
@@ -29,7 +29,7 @@ export function ArticleLayout({ article, children }: { article: ArticleWithSlug;
   return (
     <Container className="mt-16 lg:mt-32">
       <div className="xl:relative">
-        <div className="mx-auto max-w-2xl">
+        <div className="mx-auto max-w-3xl">
           {previousPathname && (
             <button
               type="button"
