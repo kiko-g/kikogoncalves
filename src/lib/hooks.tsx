2c3b9aed6f1cf8a5b017638d08b8c78a12ceebe8
kiko-g <kikojpgoncalves@gmail.com>
2024-05-26 02:02:08 +0100

    prevent ssr local storage

--- src/lib/hooks.tsx
+++ src/lib/hooks.tsx
@@ -4,12 +4,14 @@ import { useEffect, useState } from 'react'
 
 export function useLocalStorageBoolean(key: string, defaultValue: boolean) {
   const [value, setValue] = useState(() => {
+    if (typeof window !== 'undefined') return defaultValue
+
     const storedValue = localStorage.getItem(key)
     return storedValue !== null ? JSON.parse(storedValue) : defaultValue
   })
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
+    if (typeof window !== 'undefined') localStorage.setItem(key, JSON.stringify(value))
   }, [key, value])
 
   return [value, setValue] as [boolean, React.Dispatch<React.SetStateAction<boolean>>]
