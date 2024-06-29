5a11b0ad3b27608fa8eb67358b8ef29972f15fec
kiko-g <kikojpgoncalves@gmail.com>
2024-06-29 13:06:14 +0100

    rename bagr-ui to bagger-ui

--- src/images/portfolio/index.ts
+++ src/images/portfolio/index.ts
@@ -1,5 +1,5 @@
 import BaggerFlowScreenshot from './bagger-flow/screenshot.png'
-import BagrUIScreenshot from './bagr-ui/screenshot.png'
+import BagrUIScreenshot from './bagger-ui/screenshot.png'
 import GuernerScreenshot from './guerner/screenshot.jpg'
 import MafiaInTownScreenshot from './mafia-in-town/screenshot.png'
 import MafiaInTownScreenshot2 from './mafia-in-town/screenshot2.png'
