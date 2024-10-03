00ec5b859f64ceb18c643c4eb633c592bf9c877f
kiko-g <kikojpgoncalves@gmail.com>
2024-10-03 17:31:09 +0100

    add job hunt improvements

--- src/app/job-hunt/page.tsx
+++ src/app/job-hunt/page.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next'
 import { LayoutSimple } from '@/components/LayoutSimple'
 import Link from 'next/link'
+import { CopyToClipboard } from '@/components/job-hunt/CopyToClipboard'
 
 export const metadata: Metadata = {
   title: 'Job Hunt',
@@ -9,31 +10,64 @@ export const metadata: Metadata = {
 }
 
 export default function JobHunt() {
+  const jobHuntEntries = [
+    {
+      company: 'Decipad',
+      position: 'Frontend Developer',
+      description: (
+        <p>
+          I’m excited about the opportunity at Decipad because your innovative approach to digital storytelling and data
+          visualization aligns perfectly with my interests. The focus on creativity and innovation resonates with me, as
+          I love exploring new ways to create user-friendly digital tools. I believe my skills and passion for frontend
+          development match the job description well, and I’m confident I can contribute significantly to Decipad&apos;s
+          product. You can check out my technical profile and experience in my CV (
+          <Link href="kikogoncalves.com/cv.pdf" className="outer-link">
+            kikogoncalves.com/cv.pdf
+          </Link>
+          ) and portfolio (
+          <Link href="kikogoncalves.com/projects" className="outer-link">
+            kikogoncalves.com/projects
+          </Link>
+          ), which showcase projects that align well with this opportunity.
+        </p>
+      ),
+      link: 'kikogoncalves.com/cv.pdf',
+    },
+  ]
+
   return (
     <LayoutSimple
       title="Job Hunt playground"
       intro="This is a playground for me to test out different job hunt strategies and tactics. I'll be sharing my progress and learnings here."
     >
-      <section className="border border-slate-300 bg-slate-100 p-4 dark:border-slate-700 dark:bg-slate-600/20">
-        <h3 className="mb-1.5 flex-1 text-xl font-bold leading-7">Decipad</h3>
-        <div className="max-w-2xl">
-          <p>
-            I’m excited about the opportunity at Decipad because your innovative approach to digital storytelling and
-            data visualization aligns perfectly with my interests. The focus on creativity and innovation resonates with
-            me, as I love exploring new ways to create user-friendly digital tools. I believe my skills and passion for
-            frontend development match the job description well, and I’m confident I can contribute significantly to
-            Decipad&apos;s product. You can check out my technical profile and experience in my CV (
-            <Link href="kikogoncalves.com/cv.pdf" className="outer-link">
-              kikogoncalves.com/cv.pdf
-            </Link>
-            ) and portfolio (
-            <Link href="kikogoncalves.com/projects" className="outer-link">
-              kikogoncalves.com/projects
-            </Link>
-            ), which showcase projects that align well with this opportunity.
-          </p>
-        </div>
-      </section>
+      {jobHuntEntries.map((jobHuntEntry, jobHuntEntryIdx) => (
+        <JobHuntEntry key={jobHuntEntryIdx} {...jobHuntEntry} />
+      ))}
     </LayoutSimple>
+  )
+}
+
+function JobHuntEntry({
+  company,
+  position,
+  description,
+  link,
+}: {
+  company: string
+  position: string
+  description: React.ReactNode
+  link: string
+}) {
+  return (
+    <section className="border border-slate-200 bg-slate-50 p-4 dark:border-slate-700 dark:bg-slate-600/20">
+      <div className="flex items-center justify-between gap-2">
+        <div className="mb-1.5 flex flex-col">
+          <h3 className="flex-1 text-xl font-bold leading-7">{company}</h3>
+          <p className="text-sm text-slate-500 dark:text-slate-400">{position}</p>
+        </div>
+        <CopyToClipboard textTsx={description} />
+      </div>
+      <div className="max-w-2xl">{description}</div>
+    </section>
   )
 }
---
+++ src/components/job-hunt/CopyToClipboard.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React, { useCallback, useState } from 'react'
+import clsx from 'clsx'
+import { CheckIcon, ClipboardIcon } from '@heroicons/react/24/outline'
+
+export function CopyToClipboard({ textTsx }: { textTsx: React.ReactNode }) {
+  const convertedText = extractTextFromReactNode(textTsx)
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = useCallback(() => {
+    navigator.clipboard.writeText(convertedText)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 2000)
+  }, [convertedText])
+
+  return (
+    <button
+      onClick={handleCopy}
+      className={clsx(
+        'flex items-center gap-1 rounded border px-2.5 py-0.5 text-sm transition',
+        copied
+          ? 'border-green-200 bg-green-50'
+          : 'border-slate-300 bg-slate-100 hover:bg-slate-200 dark:border-slate-700 dark:bg-slate-600/20',
+      )}
+    >
+      <span>{copied ? 'Copied' : 'Copy'}</span>
+      {copied ? <CheckIcon className="h-4 w-4" /> : <ClipboardIcon className="h-4 w-4" />}
+    </button>
+  )
+}
+
+function extractTextFromReactNode(node: React.ReactNode): string {
+  if (typeof node === 'string' || typeof node === 'number') return node.toString()
+  else if (Array.isArray(node)) return node.map(extractTextFromReactNode).join('')
+  else if (React.isValidElement(node)) return extractTextFromReactNode(node.props.children)
+  else return '' // Handle other types like null, undefined, boolean, etc.
+}
