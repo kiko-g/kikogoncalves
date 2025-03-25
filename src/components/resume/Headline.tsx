6ffbbdb8d17822156f125b4cd5f5af5f832638f5
kiko-g <kikojpgoncalves@gmail.com>
2025-03-25 02:16:20 +0000

    .

--- src/components/resume/Headline.tsx
+++ src/components/resume/Headline.tsx
@@ -28,7 +28,6 @@ export function Headline() {
         <span>Francisco</span>
         <span className="ml-1.5 font-bold">Gonçalves</span>
       </h1>
-
       <div className="flex flex-col gap-1 text-sm lg:flex-row lg:gap-3 lg:text-base">
         <Bubble className="w-fit text-xs font-medium">Software Development Engineer @ Jumpseller</Bubble>
         <Bubble className="w-fit text-xs font-medium">MSc in Computer Science and Engineering</Bubble>
@@ -37,7 +36,7 @@ export function Headline() {
         </Bubble>
       </div>
 
-      <div className="mt-2 flex w-full flex-col flex-wrap gap-1 border-t pt-2 text-sm opacity-80 lg:mt-2 lg:flex-row lg:gap-4 lg:border-t-0 lg:pt-0">
+      <div className="mt-2 flex w-full flex-col flex-wrap gap-1 border-t pt-2 text-sm lg:mt-2 lg:flex-row lg:gap-4 lg:border-t-0 lg:pt-0">
         <Link href="tel:+351918271105" className="flex items-center gap-2 hover:underline lg:gap-1">
           <PhoneIcon className="size-4" />
           <span>+351 91 827 11 05</span>
@@ -53,10 +52,7 @@ export function Headline() {
           <span>https://kikogoncalves.com/projects</span>
         </Link>
 
-        <Link
-          href="https://www.linkedin.com/in/kikogoncalves/"
-          className="flex items-center gap-2 hover:underline lg:gap-1"
-        >
+        <Link href="https://linkedin.com/in/kikogoncalves" className="flex items-center gap-2 hover:underline lg:gap-1">
           <Image src={logoLinkedin} alt="Linkedin" width={16} height={16} />
           <span>kikogoncalves_</span>
         </Link>
